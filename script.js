ac2bf83dfa3a6eda2bb05576d5c449ce2fef8001
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-11 22:35:31 +0100

    update after class

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("Ready to go! 🚀");
+
+let i = 0;
+
+// ------------------------------
+// Iteration 1
+// ------------------------------
+// while (i <= 30) {
+//   console.log(i);
+//   i++;
+// }
+
+// ------------------------------
+// Iteration 2
+// ------------------------------
+// while (i <= 30) {
+//   switch (i) {
+//     case 10:
+//       console.log("ten");
+//       break;
+//     case 20:
+//       console.log("twenty");
+//       break;
+//     default:
+//       console.log(i);
+//   }
+//   i++;
+// }
