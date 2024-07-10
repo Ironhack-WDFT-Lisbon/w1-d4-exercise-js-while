e31a87b78e6a41d7912151ae6058f072e7396d9f
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-07-10 13:10:33 +0100

    update exercise jul 24

--- script.js
+++ script.js
@@ -5,24 +5,7 @@ let i = 0;
 // ------------------------------
 // Iteration 1
 // ------------------------------
-// while (i <= 30) {
-//   console.log(i);
-//   i++;
-// }
 
 // ------------------------------
 // Iteration 2
 // ------------------------------
-// while (i <= 30) {
-//   switch (i) {
-//     case 10:
-//       console.log("ten");
-//       break;
-//     case 20:
-//       console.log("twenty");
-//       break;
-//     default:
-//       console.log(i);
-//   }
-//   i++;
-// }
